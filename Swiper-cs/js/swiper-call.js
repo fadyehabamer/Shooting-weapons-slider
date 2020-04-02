8ab1c21957e3415c64a538c088076d78221b4d37
fadyehabamer <fadyamer45@gmail.com>
2020-04-02 03:34:09 +0200

    Add files via upload

---
+++ Swiper-cs/js/swiper-call.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper('.swiper-container', {
+		effect: 'coverflow',
+		grabCursor: true,
+		centeredSlides: true,
+		slidesPerView: 'auto',
+		coverflowEffect: {
+			rotate: 20,
+			stretch: 0,
+			depth: 400,
+			modifier: 1,
+			slideShadows: true,
+		},
+		pagination: {
+			el: '.swiper-pagination',
+		},
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	});
